01e676cf715941bd2e79b29dce7e8e4ec7abe250
Abuzayd <majeedroshin@gmail.com>
2017-09-30 01:07:50 +0400

    Part #5 - MongoDB/Mongoose - [Finished]
    https://www.youtube.com/watch?v=QCJCglPLUgg

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,31 @@
+const User = require('../models/user');
+
 module.exports = {
 
   signUp: async (req, res, next) => {
     // Already validated Email & Password
     // req.value.body will have the all the data
-    console.log( 'req.value.body is set as follows... ', req.value.body );
-    console.log( 'req.body is set as follows... ', req.body );
+    // console.log( 'req.value.body is set as follows... ', req.value.body );
+    // console.log( 'req.body is set as follows... ', req.body );
     console.log('UsersController.signUp() called!!!');
+
+    const { email, password } = req.value.body;
+
+    // Check if there is a user with the same email
+    const foundUser = await User.findOne({ email: email });
+    if ( foundUser ) {
+      return res.status(409).json({ error: 'Email is already in use' });
+    }
+
+    // Create a new user
+    const newUser = new User({
+      email: email,
+      password: password
+    });
+    await newUser.save();
+
+    // Respond with token
+    res.json({ user: 'created' });
   },
 
   signIn: async (req, res, next) => {
